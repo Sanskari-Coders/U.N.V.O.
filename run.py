1f8b53fe55b487d2cfb2364cdf378daabfc17a4c
Adarsh Nanda <adarshananda51@gmail.com>
2024-03-05 11:10:23 +0530

    .

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import subprocess
 import webbrowser
 webbrowser.open('http://127.0.0.1:8000/')
-subprocess.call(['python','manage.py','runserver'])
+subprocess.call(['python3','manage.py','runserver'])
