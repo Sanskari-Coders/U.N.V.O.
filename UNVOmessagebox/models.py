1b3d292c223996f21c1801ee5904ae258a6ce3e4
Anshuman Rath <anshumanrath030@gmail.com>
2024-03-05 20:55:20 +0530

    .

--- UNVOmessagebox/models.py
+++ UNVOmessagebox/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-# class Messagaes
+class Messages(models.Model):
+    mid=models.AutoField(primary_key=True)
+    message=models.CharField(max_length=50)
+    date = models.DateField(auto_now_add=True)
+    time=models.TimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.mid}"
