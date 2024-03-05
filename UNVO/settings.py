e0d8022143cff47f881c07b6222295ff1f3a4443
Anshuman Rath <anshumanrath030@gmail.com>
2024-03-05 20:12:40 +0530

    .

--- UNVO/settings.py
+++ UNVO/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-8wwna3f_5g14g8cflmu8wj*%y(-^^5n&r=(kg!yk69e+4%h*w1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'UNVOmessagebox.apps.UnvomessageboxConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import subprocess
 import webbrowser
+
 webbrowser.open('http://127.0.0.1:8000/')
 subprocess.call(['python','manage.py','runserver'])
